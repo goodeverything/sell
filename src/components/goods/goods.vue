8a718be906aa7f9356253e2ca0eaf36fe603afcd
david <easycomeeasygo55@163.com>
2017-07-16 22:16:40 +0800

    编写goods组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,117 @@
 <template>
-  <div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text">
+            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img :src="food.icon"/>
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="description">{{food.description}}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}</span>
+                </div>
+                <div class="price">
+                  <span>${{food.price}}</span>
+                  <span v-show="food.oldPrice">${{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default{};
+  import axios from 'axios';
+  const ERR_OK = 0;
+  export default{
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      axios.get('http://localhost:3030/api/goods')
+        .then(response => {
+          response = response.data;
+          if (response.errno === ERR_OK) {
+            this.goods = response.data;
+            console.log(this.goods);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        width: 56px
+        height: 54px
+        padding: 0 12px
+        line-height: 14px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border: 1px
+          font-size: 12px
+    .food-wrapper
+      flex: 1
 </style>
